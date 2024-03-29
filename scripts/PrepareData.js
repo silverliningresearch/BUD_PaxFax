79a76f69edfad0009630bfc22b7f9ede9cc426bb
lexuanbinh <lb@silverliningresearch.com>
2024-03-29 08:45:57 +0700

    update new files

--- scripts/PrepareData.js
+++ scripts/PrepareData.js
@@ -66,7 +66,10 @@ function initCurrentTimeVars() {
       break;      
     case "2024-Q1":
         total_quota = 1000;
-        break;          
+        break;      
+    case "2024-Q2":
+          total_quota = 1000;
+          break;               
     default:
       total_quota = 1;  
       break;
